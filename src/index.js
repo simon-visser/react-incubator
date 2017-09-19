7cc27bddbc1dacb48015ed5b4d505be806a00fb1
simon.visser <simon.visser0@gmail.com>
2017-09-19 08:23:00 +0200

    test

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>,awe
   document.getElementById('root'),
 );
 registerServiceWorker();
